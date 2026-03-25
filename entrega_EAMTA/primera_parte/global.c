c67e4d8184ffca75245cb9141895994503bec7b5
Bruno <brunomoreira.uy@gmail.com>
2026-03-24 23:50:26 -0300

    init_lab (config. de laboratório)

---
+++ entrega_EAMTA/primera_parte/global.c
@@ -0,0 +1,9 @@
+#include "global.h"
+#include <stdio.h>
+
+void init_lab(void) {
+    printf("Integrantes del grupo:\n");
+    printf("Nicolas Mayer\n");
+    printf("Victoria Amor\n");
+    printf("Bruno Moreira\n");
+}
---
+++ entrega_EAMTA/primera_parte/global.h
@@ -0,0 +1,6 @@
+#ifndef GLOBAL_H
+#define GLOBAL_H
+
+void init_lab(void); //Imprime los nombres de los integrantes del grupo. 
+
+#endif // GLOBAL_H
